e589ad8508920c8b0c8ff19d80c32f08fc2ae36e
dimitrii-ben <dimitrii.benoit@hotmail.com>
2024-11-12 11:52:32 +0100

    first commit

---
+++ Core/Inc/fonctions.h
@@ -0,0 +1,20 @@
+/*
+ * fonctions.h
+ *
+ *  Created on: Nov 8, 2024
+ *      Author: Nicolas SIMOND
+ */
+
+#ifndef INC_FONCTIONS_H_
+#define INC_FONCTIONS_H_
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include "stm32g4xx_hal.h"
+#include "stm32g4xx_hal_gpio.h"
+
+void setup();
+void loop();
+
+
+#endif /* INC_FONCTIONS_H_ */
---
+++ Core/Src/fonctions.c
@@ -0,0 +1,28 @@
+/*
+ * fonctions.c
+ *
+ *  Created on: Nov 8, 2024
+ *      Author: Nicolas SIMOND
+ */
+
+#include "fonctions.h"
+
+extern UART_HandleTypeDef hlpuart1;
+uint8_t prompt[15] = "\nSTM32G431 >> ";
+uint8_t message[50];
+
+void setup() {
+	strcpy((char*)message, "MSC 2024 - Capteurs\r");
+	HAL_UART_Transmit(&hlpuart1, prompt, strlen((char*)prompt), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&hlpuart1, message, strlen((char*)message), HAL_MAX_DELAY);
+}
+
+void loop(){
+	static uint32_t cnt = 0;
+	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	HAL_Delay(500);
+	snprintf((char*)message, 10, "cnt : %d\r", (int)cnt++);
+	HAL_UART_Transmit(&hlpuart1, prompt, strlen((char*)prompt), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&hlpuart1, message, strlen((char*)message), HAL_MAX_DELAY);
+}
+
