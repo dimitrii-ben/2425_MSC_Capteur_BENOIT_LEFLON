720c69d81c452e5eb53db17f630e7d9356b81048
dimitrii-ben <dimitrii.benoit@hotmail.com>
2024-11-20 08:30:27 +0100

    initSensors bis functions

--- utils/Inc/tools.h
+++ utils/Inc/tools.h
@@ -10,6 +10,10 @@
 #define MPU9250_ADDRESS 0x68
 #define WHO_AM_I_REGISTER 0x75
 #define EXPECTED_WHO_AM_I 0x71
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_1_DEFAULT 0x01
+#define RESET_VALUE 0
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -21,4 +25,7 @@ void loop();
 void test_LEDs(int);
 void verifySensor();
 uint8_t checkMPU9250Identity();
+void hardwareReset(uint8_t,uint8_t);
+void I2C_Write_Register(uint8_t, uint8_t, uint8_t);
+void InitSensors();
 #endif /* INC_TOOLS_H_ */
--- utils/Src/tools.c
+++ utils/Src/tools.c
@@ -69,5 +69,44 @@ uint8_t checkMPU9250Identity() {
 	printf("MPU-950 sensor, not detected\r\n");
 	return 0; // Sensor not detected
 }
+void I2C_Write_Register(uint8_t deviceAddress, uint8_t registerAddress, uint8_t value)
+{
+    uint8_t data[2];
+    data[0] = registerAddress; // Register address to write to
+    data[1] = value;           // Data to write to the register
+
+    // Write the register address and value to the I2C device
+    // the LSB is the R/W bit
+    // Should be set to 0 if the desired action is to write 0 otherwise
+    if (HAL_I2C_Master_Transmit(&hi2c1, deviceAddress << 1, data, 2, HAL_MAX_DELAY)!= HAL_OK){
+    	printf("Error in writing at the registerAdress [0x%02X]",registerAddress);
+    	Error_Handler();
+    }
+}
+/**
+ * @brief Reset all the registers from address1 to address2
+ * @param [adress1,adress2]: both addresses in hex
+ * @retval None
+ */
+void hardwareReset(uint8_t address1,uint8_t adress2){
+
+	for (int currentAddr = address1;currentAddr<adress2+1;currentAddr++){
+		I2C_Write_Register(MPU9250_ADDRESS, address1, RESET_VALUE);
+	}
+
+}
+void setDefaultValue(){
+	I2C_Write_Register(MPU9250_ADDRESS, WHO_AM_I_REGISTER,EXPECTED_WHO_AM_I);
+	I2C_Write_Register(MPU9250_ADDRESS, PWR_MGMT_1,PWR_MGMT_1_DEFAULT);
+}
+/**
+ * @brief Make PA5 LED blink.
+ * @param delay : current delay
+ * @retval None
+ */
+void InitSensors(){
+
+}
+
 
 
